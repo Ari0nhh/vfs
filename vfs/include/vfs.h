9a7bb68de114904d0dbd3e7ebac9e775e6a21d9f
Arion <2876866+Ari0nhh@users.noreply.github.com>
2020-03-12 22:42:58 +0500

    Interfaces

---
+++ vfs/include/vfs.h
@@ -0,0 +1,78 @@
+﻿#pragma once
+
+#include <memory>
+#include <filesystem>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <functional>
+#include <map>
+#include <mutex>
+
+#ifndef interface
+#define interface struct
+#endif
+
+namespace vfs {
+	
+	interface IFile{
+		virtual std::filesystem::path name() = 0;
+		virtual ~IFile() = default;
+	public:
+		using Ptr = std::shared_ptr<IFile>;
+		using It = std::vector<Ptr>::const_iterator;
+	};
+
+	interface IFiles {
+		virtual IFile::It begin() = 0;
+		virtual IFile::It end() = 0;
+		virtual size_t size() = 0;
+		virtual bool empty() = 0;
+		virtual ~IFiles() = default;
+	public:
+		using Ptr = std::shared_ptr<IFiles>;
+	};
+
+	/*interface IFolder;
+	
+	interface IFolders {
+		virtual IFolder::It begin() = 0;
+		virtual IFolder::It end() = 0;
+		virtual bool empty() = 0;
+		virtual size_t size() = 0;
+		virtual ~IFolders() = default;
+	public:
+		using Ptr = std::shared_ptr<IFolders>;
+	};
+
+	interface IFolder {
+	public:
+		using Ptr = std::shared_ptr<IFolder>;
+		using Ptr_ = std::weak_ptr <IFolder>;
+		using It = std::vector<Ptr>::const_iterator;
+	public:
+		virtual std::filesystem::path name() = 0;
+		virtual IFiles::Ptr files();
+		virtual IFolder::Ptr parent() = 0;
+		virtual IFolders::Ptr folders() = 0;
+		virtual ~IFolder() = default;
+	};*/
+
+	interface IFileSystem {
+		virtual std::string name() = 0;
+		virtual IFile::Ptr create_file(const std::filesystem::path&) = 0;
+		virtual IFile::Ptr open_file(const std::filesystem::path&) = 0;
+		virtual ~IFileSystem() = default;
+	public:
+		using Ptr = std::shared_ptr<IFileSystem>;
+	};
+
+	interface IVolume {
+		virtual IFileSystem::Ptr mount(const std::filesystem::path&) = 0;
+		virtual ~IVolume() = default;
+	public:
+		using Ptr = std::shared_ptr<IVolume>;
+	};
+
+	IVolume::Ptr get();
+}
---
+++ vfs/src/vfs.cpp
@@ -0,0 +1,13 @@
+﻿// vfs.cpp : Defines the entry point for the application.
+//
+#include <iostream>
+
+#include "../include/vfs.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
